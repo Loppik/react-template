bfd555dd12ab6f1177bd073301960d11d3966768
Shantyr Alexander <shantyr_qq@mail.ru>
2021-06-15 14:34:59 +0300

    added template

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import saga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(saga);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
